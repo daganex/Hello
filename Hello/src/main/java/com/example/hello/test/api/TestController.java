0cb03333772fe0ea7bed2d5d2d08cf425b0dadb9
이재현 <daganex@naver.com>
2021-11-03 17:37:21 +0900

    화면 개발 예제 생성

--- Hello/src/main/java/com/example/hello/test/api/TestController.java
+++ Hello/src/main/java/com/example/hello/test/api/TestController.java
@@ -3,10 +3,28 @@ package com.example.hello.test.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/test")
 public class TestController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
-	
+
+	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
+	public String hello(Model model) throws Exception {
+
+		logger.info("model: {}", model);
+
+		try {
+			
+			model.addAttribute("msg", "방가워!" );
+			
+		} catch (Exception e) {
+			logger.info("에러 : ", e);
+		}
+		return "hello";
+	}
 }
---
+++ Hello/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+
+<h1>${msg}</h1>
+
+</body>
+</html>
