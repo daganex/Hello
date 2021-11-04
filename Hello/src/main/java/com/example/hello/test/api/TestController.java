1f99a54eebfdef4abe6bccfe16832bd91ede7ffb
이재현 <daganex@naver.com>
2021-11-04 16:27:28 +0900

    MYSQL 연결, 기본 동작 템플릿

--- Hello/src/main/java/com/example/hello/test/api/TestController.java
+++ Hello/src/main/java/com/example/hello/test/api/TestController.java
@@ -1,26 +1,40 @@
 package com.example.hello.test.api;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.hello.test.service.dao.TestMDAO;
+import com.example.hello.test.service.vo.TestVO;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
-
+	
+	@Autowired
+	public TestMDAO testMDAO;
+	
 	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
 	public String hello(Model model) throws Exception {
 
-		logger.info("model: {}", model);
-
 		try {
+			logger.info("start=============");
 			
-			model.addAttribute("msg", "방가워!" );
+			List<TestVO> testList = testMDAO.getAll();
+			
+			model.addAttribute("test", testList);
+			
+			for(TestVO vo : testList) {
+				logger.info("vo: {}", vo);
+			}
 			
 		} catch (Exception e) {
 			logger.info("에러 : ", e);
---
+++ Hello/src/main/java/com/example/hello/test/service/dao/TestMDAO.java
@@ -0,0 +1,10 @@
+package com.example.hello.test.service.dao;
+
+import java.util.List;
+
+import com.example.hello.test.service.vo.TestVO;
+
+public interface TestMDAO {
+
+	public List<TestVO> getAll();
+}
