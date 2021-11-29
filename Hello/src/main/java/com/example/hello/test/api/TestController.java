5351dba4231b8bbc7fd0304b079008c70f6b17ac
이재현 <daganex@naver.com>
2021-11-29 15:15:33 +0900

    배포된 곳에서 AJAX 요청 실패 조치 - 012

--- Hello/src/main/java/com/example/hello/test/api/TestController.java
+++ Hello/src/main/java/com/example/hello/test/api/TestController.java
@@ -23,7 +23,7 @@ public class TestController {
 	@Autowired
 	public TestService testService;
 	
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String hello(HttpServletRequest request) throws Exception {
 
 		Map<String, Object> dataMap = new HashMap<String, Object>();
